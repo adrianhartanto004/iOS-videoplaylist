b6fe5489c816a0bb245d882bf0963b336f8748c6
Adrian Hartanto <awi_dan@yahoo.com>
2021-07-02 11:31:04 +0700

    Initial Commit

---
+++ VideoPlaylistExercise/VideoPlaylistExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlaylistExerciseApp.swift
+//  VideoPlaylistExercise
+//
+//  Created by Adrian Hartanto on 02/07/21.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlaylistExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
