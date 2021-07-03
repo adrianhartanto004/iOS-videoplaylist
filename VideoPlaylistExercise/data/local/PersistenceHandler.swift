2fb4ad4d5a7517596360b00b018cf6bb64c1ffdd
Adrian Hartanto <awi_dan@yahoo.com>
2021-07-04 02:22:37 +0700

    Separate persistence handler

---
+++ VideoPlaylistExercise/data/local/PersistenceHandler.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+class PersistenceHandler {
+
+  private static var documentsFolder: URL {
+    do {
+      return try FileManager.default.url(for: .documentDirectory,
+                                         in: .userDomainMask,
+                                         appropriateFor: nil,
+                                         create: false)
+    } catch {
+      fatalError("Can't find documents directory.")
+    }
+  }
+  private static var fileURL: URL {
+    return documentsFolder.appendingPathComponent("playlist.data")
+  }
+
+  func load(completion: @escaping (VideoListInfo) -> ()) {
+    DispatchQueue.global(qos: .background).async {
+      guard let data = try? Data(contentsOf: Self.fileURL) else {
+        fatalError()
+      }
+
+      guard let videoList = try? JSONDecoder().decode(VideoListInfo.self, from: data) else {
+        fatalError("Can't decode saved playlist data.")
+      }
+
+      completion(videoList as VideoListInfo)
+    }
+  }
+
+  func save(videoListInfo: VideoListInfo) {
+    DispatchQueue.global(qos: .background).async {
+      //      guard let videos = videoListInfo else { fatalError("Self out of scope") }
+      guard let data = try? JSONEncoder().encode(videoListInfo) else { fatalError("Error encoding data") }
+      do {
+        let outfile = Self.fileURL
+        try data.write(to: outfile)
+      } catch{
+        fatalError("Can't write to file")
+      }
+    }
+  }
+}
